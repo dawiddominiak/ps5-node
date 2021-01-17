d1b9ab8c110381ba06cc1b42b02ef61dabb91d1e
Dawid Dominiak <poczta@dawiddominiak.pl>
2021-01-17 18:08:28 +0100

    Amazon fix

--- src/adapters/amazon.adapter.ts
+++ src/adapters/amazon.adapter.ts
@@ -5,7 +5,7 @@ import { XPathAdapter } from './xpath.adapter';
 
 const AMAZON_PAGE = 'https://www.amazon.de/-/pl/dp/B08H93ZRK9/ref=sr_1_2?__mk_pl_PL=ÅMÅŽÕÑ&crid=JCYSZ5WBWC6T&dchild=1&keywords=playstation+5&qid=1610248227&sprefix=pla%2Caps%2C189&sr=8-2';
 const SHORT_LINK = 'https://amzn.to/3btXajk';
-const XPATH = '#availability > span';
+const XPATH = '#availability_feature_div > div > span';
 const UNAVAILABLE_TEXT = 'Przedmiot jest obecnie niedostępny.';
 
 export class AmazonAdapter extends XPathAdapter implements Adapter {

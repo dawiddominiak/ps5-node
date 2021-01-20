79819e55c024235221298e7a4abaabeb6120aaa1
Dawid Dominiak <poczta@dawiddominiak.pl>
2021-01-20 12:09:41 +0100

    Changes in MediaMarkt adapter

--- src/adapters/media-markt.adapter.ts
+++ src/adapters/media-markt.adapter.ts
@@ -3,10 +3,10 @@ import { Browser, Page } from 'puppeteer';
 import { Adapter } from './adapter.interface';
 import { XPathAdapter } from './xpath.adapter';
 
-const MEDIAMARKT_PAGE = 'https://mediamarkt.pl/konsole-i-gry/konsola-sony-playstation-5';
-const SHORT_LINK = 'https://bit.ly/3668crB';
-const XPATH = '.b-ofrBox_cta > div > form > div > p > span';
-const UNAVAILABLE_TEXT = 'niedostępny';
+const MEDIAMARKT_PAGE = 'https://mediamarkt.pl/konsole-i-gry/playstation-5/konsole-ps5';
+const SHORT_LINK = 'https://bit.ly/3p2YsFJ';
+const XPATH = 'h1 > span';
+const UNAVAILABLE_TEXT = '(0)';
 
 export class MediaMarktAdapter extends XPathAdapter implements Adapter {
   protected pagePromise: Promise<Page>;
